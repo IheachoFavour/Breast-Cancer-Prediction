3f1f326d60ae21afa355ff5ac0232bd84c51824f
Iheacho Favour <125609035+IheachoFavour@users.noreply.github.com>
2025-05-02 14:38:56 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import joblib
 import json
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -61,4 +62,5 @@ if __name__ == '__main__':
     # Ensure db.create_all is called within app context
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
